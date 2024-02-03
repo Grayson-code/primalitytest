894e10ff5daf355e0e0aea73302256bbd2825b68
Grayson-code <northernstarmusic10@gmail.com>
2024-02-03 11:34:27 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+<!--
+ Copyright (c) 2024 Adwaith
+ 
+ This software is released under the MIT License.
+ https://opensource.org/licenses/MIT
+-->
+
+A primality test is an algorithm to find out if the input number is prime or not.
---
+++ primality test.py
@@ -0,0 +1,23 @@
+from math import isqrt
+
+def is_prime(n: int) -> bool:
+    if n <= 3:
+        return n > 1
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    limit = isqrt(n)
+    for i in range(5, limit+1, 6):
+        if n % i == 0 or n % (i+2) == 0:
+            return False
+    return True
+
+print("This is a program to see if the given integer, be it large or small is a prime number.")
+print()
+n = eval(input("Enter the integer: "))
+
+if is_prime(n):
+    print("The Given integer is prime number")
+    print("Number -> ",n)
+else:
+    print("The given integer contains more than 2 factors")
+    print("Number -> ",n)
